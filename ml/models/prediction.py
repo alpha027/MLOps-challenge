90ea0d4bc7b550d4e37b388d85be041996f9612e
anas <anasbachiri@gmail.com>
2025-01-24 12:57:12 +0100

    Changing comment and checking if model registry is a dict

--- ml/models/prediction.py
+++ ml/models/prediction.py
@@ -5,8 +5,7 @@ from fastapi import  UploadFile
 
 
 
-# Set the model to evaluation mode
-
+# Can also be set in the app config file
 ALLOWED_CONTENT_TYPES = ["image/jpeg", "image/png"]
 
 # Define the request model
--- tests/test_model_registry_integrity.py
+++ tests/test_model_registry_integrity.py
@@ -9,6 +9,9 @@ def test_registry_integrity():
 
     registry_dict = MODEL_REGISTRY
 
+    if not isinstance(registry_dict, dict):
+        raise TypeError("Registry must be a dictionary")
+
     for k, v in registry_dict.items():
         validate_registry(v)
 
